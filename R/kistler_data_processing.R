9474b68dbb705dff97bd2608073d3b9a15861899
nbartzsch <nicobartzsch@gmail.com>
2024-11-25 14:20:13 +0100

    initial commit

---
+++ R/kistler_data_processing.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(dplyr)
+library(readr)
+
+raw_data_sens <- read.csv('data/raw_data/Medusa_01_0_001.csv')
+
+processed_data_sens <-  raw_data_sens 
+
+min_value <- min(processed_data_sens$s)
+# max_value <- max(processed_data_sens$pC)
+
+processed_data_sens$s <- processed_data_sens$s - min_value
+
+# Find the row with the maximum pC value
+# max_row <- processed_data_sens[which.max(processed_data_sens$pC), ]
+
+# Extract the time (s) corresponding to the maximum pC
+# time_at_max_pC <- max_row$s
+
+max_t <- 4.75
+min_t <- 3.45
+
+processed_data_sens <- processed_data_sens |>
+  filter(s <= max_t ) |>
+  filter(s >= min_t)
+
+min_value <- min(processed_data_sens$s)
+processed_data_sens$s <- processed_data_sens$s - min_value
+
+ggplot(processed_data_sens, aes(x = s, 
+                                y = pC))+
+  geom_path()+
+  theme_minimal()
+
+write_csv(processed_data_sens, "data/processed_data/Medusa_01/first_firing/Medusa_01_01_complete.csv")
