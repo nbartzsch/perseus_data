5bf983973de530ea7f9824b06005955b62f6a468
nbartzsch <nicobartzsch@gmail.com>
2024-12-02 13:21:21 +0100

    deflagration setpoints

--- R/kistler_data_processing.R
+++ R/kistler_data_processing.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(dplyr)
 library(readr)
 
-raw_data_sens <- read.csv('data/raw_data/Medusa_01_0_001.csv')
+raw_data_sens <- read.csv('data/processed_data/Medusa_02/detonation/firing_05/Medusa_02_detonation_05.csv')
 
 processed_data_sens <-  raw_data_sens 
 
@@ -17,19 +17,19 @@ processed_data_sens$s <- processed_data_sens$s - min_value
 # Extract the time (s) corresponding to the maximum pC
 # time_at_max_pC <- max_row$s
 
-max_t <- 4.75
-min_t <- 3.45
+max_t <- 4.75#4.45
+min_t <- 3.55
 
 processed_data_sens <- processed_data_sens |>
   filter(s <= max_t ) |>
   filter(s >= min_t)
 
-min_value <- min(processed_data_sens$s)
-processed_data_sens$s <- processed_data_sens$s - min_value
+#min_value <- min(processed_data_sens$s)
+#processed_data_sens$s <- processed_data_sens$s - min_value
 
 ggplot(processed_data_sens, aes(x = s, 
                                 y = pC))+
   geom_path()+
   theme_minimal()
 
-write_csv(processed_data_sens, "data/processed_data/Medusa_01/first_firing/Medusa_01_01_complete.csv")
+write_csv(processed_data_sens, "data/processed_data/Medusa_02/detonation/firing_05/Medusa_02_detonation_05_complete.csv")
